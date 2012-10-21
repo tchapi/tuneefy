55243cc4f578bd4539d3c5a328321d6fd788455c
tchapi <regbasket@gmail.com>
2012-10-21 22:35:40 +0200

    Check that we have a real request

--- include/api/request.php
+++ include/api/request.php
@@ -2,12 +2,12 @@
 
   // We get the request
 
-  /*
+  
   if(!isset($_SERVER['SERVER_NAME']) || $_SERVER['SERVER_NAME'] != "api.tuneefy.com"){
     header ("Location: /404");
     exit;
   }
-  */
+  
 
   if (isset($_REQUEST['m']) && $_REQUEST['m'] != "") {
   
