75b0d70da6953e16ee6fefd83c4377b5154fd6cd
tchap <regbasket@gmail.com>
2013-09-01 18:32:32 +0200

    Delete insertNewAPIClient.sql

--- include/database/sql/insertNewAPIClient.sql
+++
@@ -1,8 +0,0 @@
-SET @name     := 'gooz';
-SET @email    := 'cycogooz@gmail.com';
-SET @isActive := 1;
-
-INSERT INTO `tuneefy_prod`.`api_clients` 
-  (`id`, `name`, `contact`, `consumer_key`, `consumer_secret`, `active`, `creation_date`) 
-VALUES 
-  (NULL, @name, @email, MD5(UUID()), SHA1(RAND()), @isActive, NOW());
