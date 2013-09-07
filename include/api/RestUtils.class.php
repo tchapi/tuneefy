0d815f161f4ee17f34cf2a8f133dcb4cacdf1f0d
tchapi <regbasket@gmail.com>
2013-09-07 16:15:53 +0200

    Fix indentation

--- include/api/RestUtils.class.php
+++ include/api/RestUtils.class.php
@@ -11,22 +11,22 @@ class RestUtils
 {
 
   public static function processRequest()
-	{
-		// What is the method used ?
-		$request_method = strtolower($_SERVER['REQUEST_METHOD']);
-		$request = new RestRequest();
-		
-		// Storing the requested data
-		$data = array();
+  {
+    // What is the method used ?
+    $request_method = strtolower($_SERVER['REQUEST_METHOD']);
+    $request = new RestRequest();
+    
+    // Storing the requested data
+    $data = array();
 
-		switch ($request_method)
-		{
-			
-			/* *** GET *** */
-			case 'get':
-				
-				// Because of the htaccess ...
-				// Extra parameters in the request URI
+    switch ($request_method)
+    {
+      
+      /* *** GET *** */
+      case 'get':
+        
+        // Because of the htaccess ...
+        // Extra parameters in the request URI
         $params = array();
         $parts = preg_split("~=|&~", $_SERVER['REQUEST_URI'], -1, PREG_SPLIT_NO_EMPTY); 
         
@@ -47,34 +47,34 @@ class RestUtils
         $data = $params; 
         $request->validateRequest();
         break;
-			
-			/* *** POST *** */
-			case 'post':
-				$data = $_POST;
+      
+      /* *** POST *** */
+      case 'post':
+        $data = $_POST;
         $request->invalidateRequest();
-				break;
-			
-			/* *** PUT *** */
-			case 'put':
-				// basically, we read a string from PHP's special input location,
-				// and then parse it out into an array via parse_str... per the PHP docs:
-				// Parses str  as if it were the query string passed via a URL and sets
-				// variables in the current scope.
-				parse_str(file_get_contents('php://input'), $put_vars);
-				$data = $put_vars;
+        break;
+      
+      /* *** PUT *** */
+      case 'put':
+        // basically, we read a string from PHP's special input location,
+        // and then parse it out into an array via parse_str... per the PHP docs:
+        // Parses str  as if it were the query string passed via a URL and sets
+        // variables in the current scope.
+        parse_str(file_get_contents('php://input'), $put_vars);
+        $data = $put_vars;
         $request->invalidateRequest();
-				break;
-		}
-		
+        break;
+    }
+    
     // Storing the data into data (redundant for GET, but well)
     $request->setData((object)$data);
 
-		// Storing the method used
-		$request->setMethod($request_method);
+    // Storing the method used
+    $request->setMethod($request_method);
 
-		// Set the raw data, so we can access it if needed (there may be
-		// other pieces to your requests)
-		$request->setRequestVars($data);
+    // Set the raw data, so we can access it if needed (there may be
+    // other pieces to your requests)
+    $request->setRequestVars($data);
     
     // If we have data (as per in a post request), we store it
     if(isset($data['data']))  
@@ -89,33 +89,33 @@ class RestUtils
       $request->setHttpAccept($data['alt']);
     }
      
-		return $request;
-	}
+    return $request;
+  }
 
   public static function sendResponse($status = 200, $body = '', $content_type = 'json', $apiMode = true, $json_key = null)
-	{
-	  $messages = RestUtils::getStatusCodeMessage($status);
-		
-		// Set the status	
-		$status_header = 'HTTP/1.1 ' . $status . ' ' . $messages['official'];
-		header($status_header);
-		
-		// We need to create the body if none is passed (== error)
-		if ($body === '' || $body === null)
-		{
+  {
+    $messages = RestUtils::getStatusCodeMessage($status);
+    
+    // Set the status  
+    $status_header = 'HTTP/1.1 ' . $status . ' ' . $messages['official'];
+    header($status_header);
+    
+    // We need to create the body if none is passed (== error)
+    if ($body === '' || $body === null)
+    {
 
-			$body = array("code" => $status, "message" => $messages['message']);
+      $body = array("code" => $status, "message" => $messages['message']);
 
-		}
+    }
 
-		// And we can send the response
+    // And we can send the response
 
     if ($content_type == 'json'){
-	  		        
+                
       /* ******************************* */
       //            JSON                 //
       /* ******************************* */
-    		
+        
       if ($apiMode == true) {
         // Now we add some metadata
         $response = array("provider" => "tuneefy", 
@@ -133,14 +133,14 @@ class RestUtils
       
       echo json_encode($response);
 
-	  } else if ($content_type == 'xml'){
-	      
+    } else if ($content_type == 'xml'){
+        
       /* ******************************* */
       //            XML                  //
-      /* ******************************* */  		  
+      /* ******************************* */        
       
-  		// Sets the content type
-  		header("Content-type: text/xml; charset=UTF-8 ");
+      // Sets the content type
+      header("Content-type: text/xml; charset=UTF-8 ");
           
       $xml = new XMLHelper();
       
@@ -178,61 +178,61 @@ class RestUtils
       $xml->pop(); // response
 
       echo utf8_encode($xml->getXml());
-	  
-	  }
-	  
-		exit;
-	}
+    
+    }
+    
+    exit;
+  }
 
 
-	public static function getStatusCodeMessage($status)
-	{
-		$codes = Array(
-		    100 => array("official" => 'Continue', "message" => 'Let me go on'),
-		    101 => array("official" => 'Switching Protocols', "message" => 'Crossing wires'),
-		    200 => array("official" => 'OK', "message" => 'Great job!'),
-		    201 => array("official" => 'Created', "message" => 'Done'),
-		    202 => array("official" => 'Accepted', "message" => 'Challenge: accepted'),
-		    203 => array("official" => 'Non-Authoritative Information', "message" => 'Are you sure ?'),
-		    204 => array("official" => 'No Content', "message" => 'Ain\'t nuthin\''),
-		    205 => array("official" => 'Reset Content', "message" => 'From scratch'),
-		    206 => array("official" => 'Partial Content', "message" => 'It seems like something is missing here'),
-		    300 => array("official" => 'Multiple Choices', "message" => 'Too many choices'),
-		    301 => array("official" => 'Moved Permanently', "message" => 'We\'ve moved'),
-		    302 => array("official" => 'Found', "message" => 'Fantastic'),
-		    303 => array("official" => 'See Other', "message" => 'Ask another chap'),
-		    304 => array("official" => 'Not Modified', "message" => 'Haven\'t touched it, trust me'),
-		    305 => array("official" => 'Use Proxy', "message" => 'Do use a proxy, for fuck\'s sake'),
-		    306 => array("official" => '(Unused)', "message" => '(Unused. Yet)'),
-		    307 => array("official" => 'Temporary Redirect', "message" => 'Redirecting you in a moment'),
-		    400 => array("official" => 'Bad Request', "message" => 'That is not a way indeed to formulate a query, gentleman.'),
-		    401 => array("official" => 'Unauthorized', "message" => 'Thou shall not trespass'),
-		    402 => array("official" => 'Payment Required', "message" => 'Money. Now.'),
-		    403 => array("official" => 'Forbidden', "message" => 'Na na na'),
-		    404 => array("official" => 'Not Found', "message" => 'Really sorry but this thing is gone'),
-		    405 => array("official" => 'Method Not Allowed', "message" => 'You can\'t do this I\'m sorry !'),
-		    406 => array("official" => 'Not Acceptable', "message" => 'I cannot let you say this'),
-		    407 => array("official" => 'Proxy Authentication Required', "message" => 'What credentials ?'),
-		    408 => array("official" => 'Request Timeout', "message" => 'Zzzzzzzz'),
-		    409 => array("official" => 'Conflict', "message" => 'War!'),
-		    410 => array("official" => 'Gone', "message" => 'Sorry bro, gone.'),
-		    411 => array("official" => 'Length Required', "message" => 'What\'s the size of this, may I beg you?'),
-		    412 => array("official" => 'Precondition Failed', "message" => 'Precondition Failed'),
-		    413 => array("official" => 'Request Entity Too Large', "message" => 'That is way too big, sir'),
-		    414 => array("official" => 'Request-URI Too Long', "message" => 'That is far too long, sir'),
-		    415 => array("official" => 'Unsupported Media Type', "message" => 'I can\'t handle this'),
-		    416 => array("official" => 'Requested Range Not Satisfiable', "message" => 'I\'m not satisfied with your range'),
-		    417 => array("official" => 'Expectation Failed', "message" => 'I deserved better'),
-		    500 => array("official" => 'Internal Server Error', "message" => 'Christ on a bike !'),
-		    501 => array("official" => 'Not Implemented', "message" => 'Gimme a minute to code this'),
-		    502 => array("official" => 'Bad Gateway', "message" => 'You\'re being naughty!'),
-		    503 => array("official" => 'Service Unavailable', "message" => 'We\'ll be back soon'),
-		    504 => array("official" => 'Gateway Timeout', "message" => 'Too late, bro'),
-		    505 => array("official" => 'HTTP Version Not Supported', "message" => 'Are you living in the eighties?')
-		);
+  public static function getStatusCodeMessage($status)
+  {
+    $codes = Array(
+        100 => array("official" => 'Continue', "message" => 'Let me go on'),
+        101 => array("official" => 'Switching Protocols', "message" => 'Crossing wires'),
+        200 => array("official" => 'OK', "message" => 'Great job!'),
+        201 => array("official" => 'Created', "message" => 'Done'),
+        202 => array("official" => 'Accepted', "message" => 'Challenge: accepted'),
+        203 => array("official" => 'Non-Authoritative Information', "message" => 'Are you sure ?'),
+        204 => array("official" => 'No Content', "message" => 'Ain\'t nuthin\''),
+        205 => array("official" => 'Reset Content', "message" => 'From scratch'),
+        206 => array("official" => 'Partial Content', "message" => 'It seems like something is missing here'),
+        300 => array("official" => 'Multiple Choices', "message" => 'Too many choices'),
+        301 => array("official" => 'Moved Permanently', "message" => 'We\'ve moved'),
+        302 => array("official" => 'Found', "message" => 'Fantastic'),
+        303 => array("official" => 'See Other', "message" => 'Ask another chap'),
+        304 => array("official" => 'Not Modified', "message" => 'Haven\'t touched it, trust me'),
+        305 => array("official" => 'Use Proxy', "message" => 'Do use a proxy, for fuck\'s sake'),
+        306 => array("official" => '(Unused)', "message" => '(Unused. Yet)'),
+        307 => array("official" => 'Temporary Redirect', "message" => 'Redirecting you in a moment'),
+        400 => array("official" => 'Bad Request', "message" => 'That is not a way indeed to formulate a query, gentleman.'),
+        401 => array("official" => 'Unauthorized', "message" => 'Thou shall not trespass'),
+        402 => array("official" => 'Payment Required', "message" => 'Money. Now.'),
+        403 => array("official" => 'Forbidden', "message" => 'Na na na'),
+        404 => array("official" => 'Not Found', "message" => 'Really sorry but this thing is gone'),
+        405 => array("official" => 'Method Not Allowed', "message" => 'You can\'t do this I\'m sorry !'),
+        406 => array("official" => 'Not Acceptable', "message" => 'I cannot let you say this'),
+        407 => array("official" => 'Proxy Authentication Required', "message" => 'What credentials ?'),
+        408 => array("official" => 'Request Timeout', "message" => 'Zzzzzzzz'),
+        409 => array("official" => 'Conflict', "message" => 'War!'),
+        410 => array("official" => 'Gone', "message" => 'Sorry bro, gone.'),
+        411 => array("official" => 'Length Required', "message" => 'What\'s the size of this, may I beg you?'),
+        412 => array("official" => 'Precondition Failed', "message" => 'Precondition Failed'),
+        413 => array("official" => 'Request Entity Too Large', "message" => 'That is way too big, sir'),
+        414 => array("official" => 'Request-URI Too Long', "message" => 'That is far too long, sir'),
+        415 => array("official" => 'Unsupported Media Type', "message" => 'I can\'t handle this'),
+        416 => array("official" => 'Requested Range Not Satisfiable', "message" => 'I\'m not satisfied with your range'),
+        417 => array("official" => 'Expectation Failed', "message" => 'I deserved better'),
+        500 => array("official" => 'Internal Server Error', "message" => 'Christ on a bike !'),
+        501 => array("official" => 'Not Implemented', "message" => 'Gimme a minute to code this'),
+        502 => array("official" => 'Bad Gateway', "message" => 'You\'re being naughty!'),
+        503 => array("official" => 'Service Unavailable', "message" => 'We\'ll be back soon'),
+        504 => array("official" => 'Gateway Timeout', "message" => 'Too late, bro'),
+        505 => array("official" => 'HTTP Version Not Supported', "message" => 'Are you living in the eighties?')
+    );
 
-		return (isset($codes[$status])) ? $codes[$status] : '';
-	}
+    return (isset($codes[$status])) ? $codes[$status] : '';
+  }
 }
 
 /* *************************************************** */
@@ -243,77 +243,77 @@ class RestUtils
 
 class RestRequest
 {
-	private $request_vars;
-	private $data;
-	private $http_accept;
-	private $method;
-	private $is_authorised;
+  private $request_vars;
+  private $data;
+  private $http_accept;
+  private $method;
+  private $is_authorised;
 
-	public function __construct()
-	{
-		$this->request_vars		= array();
-		$this->data				    = '';
-		
-		if (isset($_SERVER['HTTP_ACCEPT']))
-		  $this->http_accept		= (strpos($_SERVER['HTTP_ACCEPT'], 'json')) ? 'json' : 'xml';
-		else $this->http_accept = 'xml';
-		
-		$this->method			    = 'get';
-		$this->is_authorised  = false;
-	}
-
-	public function setData($data)
-	{
-		$this->data = $data;
-	}
-
-	public function setMethod($method)
-	{
-		$this->method = $method;
-	}
-
-	public function setRequestVars($request_vars)
-	{
-		$this->request_vars = $request_vars;
-	}
-	
-	public function setHttpAccept($http_accept)
-	{
-		$this->http_accept = $http_accept;
-	}
-	
-	public function validateRequest()
-	{
-		$this->is_authorised = true;
-	}
-	
-	public function invalidateRequest()
-	{
-		$this->is_authorised = false;
-	}
-	
-	public function isAuthorised()
-	{
-	  return $this->is_authorised;
+  public function __construct()
+  {
+    $this->request_vars    = array();
+    $this->data            = '';
+    
+    if (isset($_SERVER['HTTP_ACCEPT']))
+      $this->http_accept    = (strpos($_SERVER['HTTP_ACCEPT'], 'json')) ? 'json' : 'xml';
+    else $this->http_accept = 'xml';
+    
+    $this->method          = 'get';
+    $this->is_authorised  = false;
   }
 
-	public function getData()
-	{
-		return $this->data;
-	}
+  public function setData($data)
+  {
+    $this->data = $data;
+  }
 
-	public function getMethod()
-	{
-		return $this->method;
-	}
+  public function setMethod($method)
+  {
+    $this->method = $method;
+  }
 
-	public function getHttpAccept()
-	{
-		return $this->http_accept;
-	}
+  public function setRequestVars($request_vars)
+  {
+    $this->request_vars = $request_vars;
+  }
+  
+  public function setHttpAccept($http_accept)
+  {
+    $this->http_accept = $http_accept;
+  }
+  
+  public function validateRequest()
+  {
+    $this->is_authorised = true;
+  }
+  
+  public function invalidateRequest()
+  {
+    $this->is_authorised = false;
+  }
+  
+  public function isAuthorised()
+  {
+    return $this->is_authorised;
+  }
 
-	public function getRequestVars()
-	{
-		return $this->request_vars;
-	}
+  public function getData()
+  {
+    return $this->data;
+  }
+
+  public function getMethod()
+  {
+    return $this->method;
+  }
+
+  public function getHttpAccept()
+  {
+    return $this->http_accept;
+  }
+
+  public function getRequestVars()
+  {
+    return $this->request_vars;
+  }
 }
