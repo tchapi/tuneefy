f7c08d89795f3fe8b373491b4a0d7c38ef178882
tchapi <regbasket@gmail.com>
2014-06-05 18:06:12 +0200

    Quick fix for SQL fixtures

--- include/database/sql/transition5.sql
+++ include/database/sql/transition5.sql
@@ -2,6 +2,6 @@
 -- Beats Music
 
 INSERT INTO `platforms` (`id`, `name`) VALUES
-(14, 'Beats Music');
+(15, 'Beats Music');
 
 ALTER TABLE `items` ADD `link_BEATS` VARCHAR( 256 ) NULL DEFAULT NULL AFTER `link_XBOX`;
